2c6ecfa674a454d03d14556b9f520a490adbac85
taroosuga <copernix0@gmail.com>
2020-04-14 01:18:25 +0900

    日本時間修正

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Sugabook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
     g.assets false
     g.helper false
